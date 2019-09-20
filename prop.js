7e652304072d9894dc7215a5d7c2a40b5a980a6d
iaoongin <389631347@qq.com>
2019-09-20 21:59:08 +0800

    fix to runnable

--- prop.js
+++ prop.js
@@ -1,9 +1,9 @@
 var prop = {
     server: {
         // 服务器地址
-        // ip: 'http://localhost:6001',
+         ip: 'http://localhost:6001',
         // 热点调试
-        ip: 'http://192.168.137.1:6001',
+        // ip: 'http://192.168.137.1:6001',
     },
     lbs:{
         key: 'Q4GBZ-FAOK5-PUOIM-QXSX6-V6CUV-XYB7P'
